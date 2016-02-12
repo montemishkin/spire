363cde3c38199020134ba1115cd5561ec9a0efe8
Monte Mishkin <monte@mishkin.com>
2016-02-11 21:54:28 -0800

    added babel-polyfill

--- TODO.md
+++ TODO.md
@@ -1,4 +1,3 @@
-- do you not need babel-core/polyfill anymore?
 - button to download canvas image
 
 - google analytics (or does github offer some analytics on github.io pages?)
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // fix browser land
-// import 'babel-core/polyfill'
+import 'babel-polyfill'
 // third party imports
 import React from 'react'
 import ReactDOM from 'react-dom'
