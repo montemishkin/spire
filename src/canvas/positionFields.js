fd608dbfb8638fea6a045ed5a51027c6d4b20f31
Monte Mishkin <monte@mishkin.com>
2016-02-13 14:23:02 -0800

    combined bouce and suck position fields

--- src/canvas/positionFields.js
+++ src/canvas/positionFields.js
@@ -3,26 +3,28 @@ import * as vector2 from 'util/vector2'
 
 
 export default [
-    // bounce off walls
+    // bouce off walls / suck towards mouse
     (state, dot) => {
-        const [x, y] = dot.p
-        const r = dot.r
-        const width = state.browser.width
-        const height = state.browser.height
+        // bounce off walls
+        if (state.mouse.isUp) {
+            const [x, y] = dot.p
+            const r = dot.r
+            const width = state.browser.width
+            const height = state.browser.height
 
-        let v = dot.v
-        v = x - r < 0 || x + r > width ? vector2.flipX(v) : v
-        v = y - r < 0 || y + r > height ? vector2.flipY(v) : v
+            let v = dot.v
+            v = x - r < 0 || x + r > width ? vector2.flipX(v) : v
+            v = y - r < 0 || y + r > height ? vector2.flipY(v) : v
 
-        return v
-    },
-    // suck towards mouse
-    (state, dot) => {
-        const mouse = [state.mouse.x, state.mouse.y]
+            return v
+        // suck towards mouse
+        } else {
+            const mouse = [state.mouse.x, state.mouse.y]
 
-        return vector2.scale(50, vector2.normalize(
-            vector2.subtract(mouse, dot.p)
-        ))
+            return vector2.scale(50, vector2.normalize(
+                vector2.subtract(mouse, dot.p)
+            ))
+        }
     },
     // squigle pod flow
     (state, dot) => {
